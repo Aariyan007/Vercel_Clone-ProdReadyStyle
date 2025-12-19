4c8689127d00da989e266aeaf7ea0add345b1366
Aariyan007 <aariyansunu28@gmail.com>
2025-12-19 23:44:52 +0530

    added end directory

---
+++ build-server/script.js
@@ -0,0 +1,10 @@
+const {exec} = require('child_process');
+const path = require('path');
+// const path = require(path)
+
+
+async function init(){
+    console.log("Hello Universe");
+
+    const outDirPath = path.join(__dirname,'output')
+}
